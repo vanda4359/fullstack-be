667d537769282ec1ba1275f7cfdceddda909f602
vanda <vandait996@gmail.com>
2024-12-17 23:02:54 +0700

    update field user

--- src/services/userService.js
+++ src/services/userService.js
@@ -74,14 +74,14 @@ let getAllUsers = (userId) => {
     return new Promise(async (resolve, reject) => {
         try {
             let users = '';
-            if (userId === 'All') {
+            if (userId === 'ALL') {
                 users = await db.User.findAll({
                     attributes: {
                         exclude: ['password']
                     }
                 })
             }
-            if (userId && userId !== 'All') {
+            if (userId && userId !== 'ALL') {
                 users = await db.User.findOne({
                     where: { id: userId },
                     attributes: {
@@ -156,7 +156,7 @@ let deleteUser = (userId) => {
 let updateUserData = (data) => {
     return new Promise(async (resolve, reject) => {
         try {
-            if(!data.id) {
+            if(!data.id || !data.roleId || !data.positionId || !data.gender) {
                 resolve({
                     errCode: 2,
                     errMessage: `Missing requied parameter!`
@@ -170,6 +170,10 @@ let updateUserData = (data) => {
                 user.firstName = data.firstName;
                 user.lastName = data.lastName;
                 user.address = data.address;
+                user.roleId = data.roleId;
+                user.positionId = data.positionId;
+                user.gender = data.gender;
+                user.phoneNumber = data.phoneNumber;
 
                 await user.save();
 
